e29fa113d9bbe899a8e9f2047a85404274e461bb
banana090109 <vanapotopalskij@gmail.com>
2025-10-21 21:34:42 +0300

    Refactor code

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 def is_even(num: int) -> bool:
     return num % 2 == 0
 
+
 def main():
     print(f"is_even(4): {is_even(4)}")
     print(f"is_even(5): {is_even(5)}")
-    
+
+
 if __name__ == "__main__":
     main()
--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from app import is_even
 
+
 def test_is_even():
     assert not is_even(1)
     assert is_even(2)
